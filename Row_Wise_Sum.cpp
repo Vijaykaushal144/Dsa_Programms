c7f73268c4e3414f05f08e9c8773abddca0477ce
vijay kaushal <vijaykaushal144@gmail.com>
2022-02-05 18:37:34 +0530

    sum of row

--- Row_Wise_Sum.cpp
+++ Row_Wise_Sum.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
-void sum_row(int a[][3],int i,int j)
-{   cout<<"Printing sum of  row== "<<endl;
-     for(int i=0;i<3;i++)
+void sum_column(int a[][3],int i,int j)
+{   cout<<" sum of  column == "<<endl;
+     for(int j=0;j<3;j++)
     {int sum=0;
-        for(int j=0;j<3;j++)
+        for(int i=0;i<3;i++)
         {
            sum+=a[i][j];
         }
@@ -12,10 +12,45 @@ void sum_row(int a[][3],int i,int j)
     }
     cout<<endl;
 }
+void  sum_row(int a[][3],int i,int j)
+{   cout<<" sum of  row== "<<endl;
+int sum=0;
+     for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+           sum+=a[i][j];
+        }
+        cout<<sum<<" ";
+    }
+    cout<< endl;
+    
+
+
+}
+void  Max_in_sum_row(int a[][3],int i,int j)
+{   
+int sum=0;
+     for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+           sum+=a[i][j];
+        }
+    }
+    int max=-1;
+    if(sum>max)
+    {
+        max=sum;
+    }
+    cout<<"max of sum is =="<<max<<endl;
+
+
+}
 int main()
 {
      int arr[3][3];
-     cout<<"Enter the element of array";
+     cout<<"Enter the element of array"<<endl;
     for(int i=0;i<3;i++)
     {
         for(int j=0;j<3;j++)
@@ -24,6 +59,8 @@ int main()
         }
     }
     sum_row(arr ,3,3);
+    sum_column(arr,3,3);
+    Max_in_sum_row(arr,3,3);
 
 
 }
