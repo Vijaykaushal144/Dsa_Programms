c6b18f02a15b04178b346a9c439c86d808cab79d
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2022-02-21 15:23:09 +0530

    Fibonacci Number(Leetcode)

---
+++ fabonacci_Recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int fib(int n) {
+        if(n==0)    return 0;
+        
+        if(n==1)     return 1;
+        
+        int result=fib(n-1) +fib(n-2);
+     return result;   
+   
+}
+int main()
+{
+int n;
+cin>>n;
+cout<<fib(n);
+}
